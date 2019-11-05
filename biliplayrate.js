58bb8d695b1ab6df68b9b60c4ed8d25109c71853
KUAI <2542862169@qq.com>
2019-11-05 15:13:32 +0800

    fit new ui

--- biliplayrate.js
+++ biliplayrate.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili视频自定义播放速度
 // @namespace    kuai
-// @version      1.10
+// @version      1.20
 // @description  bilibili视频自定义播放速度
 // @author       kuai
 // @include      /^https?:\/\/www\.bilibili\.com\/.*
@@ -12,20 +12,30 @@
 
 (function() {
     'use strict';
-    var speedcontrol = document.createElement("span");
-    var input = document.createElement("input");
-    input.type = "text";
-    input.style.width = "76px";
-    input.onkeypress = function(e){
-        if(e.keyCode==13){
-            var v = document.querySelectorAll("video");
-            for (var i = 0; i < v.length; i++) {
-                v[i].playbackRate=input.value;
+    var addedFunc =false;
+    function readyt(){
+        document.removeEventListener('scroll', readyt);
+        var speedcontrol = document.createElement("span");
+        var input = document.createElement("input");
+        input.type = "text";
+        input.style.width = "76px";
+        input.onkeypress = function(e){
+            if(e.keyCode==13){
+                var v = document.querySelectorAll("video");
+                for (var i = 0; i < v.length; i++) {
+                    v[i].playbackRate=input.value;
+                }
             }
-        }
-    };
-    speedcontrol.appendChild(input);
-    speedcontrol.style.position="fixed";
-    speedcontrol.style.top="50%";
-    document.querySelectorAll("body")[0].appendChild(speedcontrol);
+        };
+        speedcontrol.appendChild(input);
+        speedcontrol.style.position="fixed";
+        speedcontrol.style.top="50%";
+        document.querySelectorAll("body")[0].appendChild(speedcontrol);
+    }
+    document.addEventListener('scroll',readyt);
+    document.addEventListener('scroll',function(){
+        document.querySelector(".bilibili-player-video-btn-widescreen").style.setProperty('display', 'none', 'important');
+        
+    });
+    
 })();
