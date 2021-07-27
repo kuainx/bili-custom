e6f886fadcdd7751718d9440624a37e536eac479
KUAI <2542862169@qq.com>
2021-07-27 13:20:48 +0800

    player v3

--- biliplayrate.js
+++ biliplayrate.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili视频自定义脚本
 // @namespace    kuai
-// @version      2.0
+// @version      2.1
 // @description  自定义播放速度，隐藏宽屏，滚动条自定义
 // @author       kuai
 // @include      /^https?:\/\/www\.bilibili\.com\/.*
@@ -15,25 +15,25 @@
 // ==/UserScript==
 
 
-(function() {
-    'use strict';
-    function scrollStyle() {
-        if (document.getElementById("scroollBarCustom")) {
-            return;
-        }
-        let styleDOM = document.getElementById("bilibili-evolved-variables");
-        if (!styleDOM) {
-            return;
-        }
-        let s = document.createElement("style");
-        s.id = "scroollBarCustom";
-        s.innerHTML = `.bui-bar.bui-bar-buffer{
+(function () {
+  'use strict';
+  function scrollStyle() {
+    if (document.getElementById("scroollBarCustom")) {
+      return;
+    }
+    let styleDOM = document.getElementById("bilibili-evolved-variables");
+    if (!styleDOM) {
+      return;
+    }
+    let s = document.createElement("style");
+    s.id = "scroollBarCustom";
+    s.innerHTML = `.bui-bar.bui-bar-buffer,.squirtle-progress-buffer{
 			background-color: rgb(0, 255, 0) !important;
 			opacity: 1!important;
 		}`;
-        styleDOM.parentNode.insertBefore(s, styleDOM);
-        document.removeEventListener("scroll", scrollStyle);
-    }
-    document.addEventListener("scroll", scrollStyle);
-    scrollStyle();
+    styleDOM.parentNode.insertBefore(s, styleDOM);
+    document.removeEventListener("scroll", scrollStyle);
+  }
+  document.addEventListener("scroll", scrollStyle);
+  scrollStyle();
 })();
