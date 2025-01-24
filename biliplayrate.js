f45584ede547e6c18653d093f01b0a8612cfbc4f
KUAI <2542862169@qq.com>
2025-01-24 19:56:21 +0800

    Update biliplayrate.js

--- biliplayrate.js
+++ biliplayrate.js
@@ -1,39 +1,31 @@
 // ==UserScript==
 // @name         Bilibili视频自定义脚本
 // @namespace    kuai
-// @version      2.1
-// @description  自定义播放速度，隐藏宽屏，滚动条自定义
+// @version      2.2
+// @description  滚动条自定义，隐藏宽屏
 // @author       kuai
 // @include      /^https?:\/\/www\.bilibili\.com\/.*
-// @include      /^https?:\/\/t\.bilibili\.com\/.*
-// @include      /^https?:\/\/live\.bilibili\.com\/.*
-// @include      /^https?:\/\/space\.bilibili\.com\/.*
-// @include      /^https?:\/\/search\.bilibili\.com\/.*
 // @include      /^https?:\/\bilibili\.com\/.*
 // @grant        none
 // @license      MIT License
 // ==/UserScript==
 
-
 (function () {
   'use strict';
   function scrollStyle() {
-    if (document.getElementById("scroollBarCustom")) {
+    console.log('ScrollStyle');
+    if (document.getElementById('scroollBarCustom')) {
       return;
     }
-    let styleDOM = document.getElementById("bilibili-evolved-variables");
-    if (!styleDOM) {
-      return;
-    }
-    let s = document.createElement("style");
-    s.id = "scroollBarCustom";
-    s.innerHTML = `.bui-bar.bui-bar-buffer,.squirtle-progress-buffer{
+    let s = document.createElement('style');
+    s.id = 'scroollBarCustom';
+    s.innerHTML = `.bpx-player-progress-schedule-buffer{
 			background-color: rgb(0, 255, 0) !important;
 			opacity: 1!important;
-		}`;
-    styleDOM.parentNode.insertBefore(s, styleDOM);
-    document.removeEventListener("scroll", scrollStyle);
+		}.bpx-player-ctrl-wide{
+			display: none !important;
+    }`;
+    document.body.appendChild(s);
   }
-  document.addEventListener("scroll", scrollStyle);
-  scrollStyle();
+  setTimeout(scrollStyle, 1000);
 })();
